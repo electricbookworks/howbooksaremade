38c37aa1bbb4ac9fa5e46a1e336924442b1db344
Arthur Attwell <arthur@arthurattwell.com>
2021-01-13 17:06:28 +0200

    Add EBW credit, refine transcript design

--- _includes/transcripts.js
+++ _includes/transcripts.js
@@ -9,12 +9,22 @@ function formatTranscriptHeads() {
     if (document.body.getAttribute('data-episode', 'true')) {
         console.log('ep page');
 
-        var timestampRegex = /(<strong>.*<\/strong>(\s|\n)*\d+:\d+(\s|\n)*<br.*>)/;
+        var nameAndTimestampRegex = /(<strong>.*<\/strong>(\s|\n)*\d+:\d+(\s|\n)*<br.*>)/;
+        var timestampRegex = /(\d+:\d+)/;
         var paras = document.querySelectorAll('p');
 
         paras.forEach(function (para) {
+
+            // Get the HTML to transform
             var oldHTML = para.innerHTML;
-            var newHTML = oldHTML.replace(timestampRegex, '<span class="timestamp">$1</span>');
+
+            // Wrap the name and timestamp in a span
+            var newHTML = oldHTML.replace(nameAndTimestampRegex, '<span class="name-and-timestamp">$1</span>');
+
+            // Wrap the timestamp in its own span
+            var newHTML = newHTML.replace(timestampRegex, '<span class="timestamp">$1</span>');
+
+            // Replace the old HTML with the new HTML
             para.innerHTML = newHTML;
         });
     }
