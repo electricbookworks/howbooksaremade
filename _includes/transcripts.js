b86ffdbd9ac9fbf9c6aefea5b1c441fb57a11f3c
Arthur Attwell <arthur@arthurattwell.com>
2024-06-03 16:19:12 +0200

    Update JS and CSS for new transcript HTML

--- _includes/transcripts.js
+++ _includes/transcripts.js
@@ -1,35 +1,62 @@
-/*jslint browser */
+// Format transcript name headings and timestamps nicely.
+// This function handles legacy formatting for original episodes.
+function formatOldTranscriptHeads () {
+  'use strict'
 
-// Format transcript name headings and timestamps nicely
-function formatTranscriptHeads() {
-    'use strict';
+  // If this is an episode page, wrap the names and timestamps
+  // in a span that we can target with CSS.
+  if (document.body.getAttribute('data-episode', 'true')) {
+    const nameAndTimestampRegex = /(<strong>.*<\/strong>(\s|\n)*\d+:\d+(\s|\n)*<br.*>)/
+    const timestampRegex = /(\d+:\d+)/
+    const paras = document.querySelectorAll('p')
 
-    // If this is an episode page, wrap the names and timestamps
-    // in a span that we can target with CSS.
-    if (document.body.getAttribute('data-episode', 'true')) {
-        console.log('ep page');
+    paras.forEach(function (para) {
+      // Get the HTML to transform
+      const oldHTML = para.innerHTML
 
-        var nameAndTimestampRegex = /(<strong>.*<\/strong>(\s|\n)*\d+:\d+(\s|\n)*<br.*>)/;
-        var timestampRegex = /(\d+:\d+)/;
-        var paras = document.querySelectorAll('p');
+      // Wrap the name and timestamp in a span
+      let newHTML = oldHTML.replace(nameAndTimestampRegex, '<span class="name-and-timestamp">$1</span>')
 
-        paras.forEach(function (para) {
+      // Wrap the timestamp in its own span
+      newHTML = newHTML.replace(timestampRegex, '<span class="timestamp">$1</span>')
 
-            // Get the HTML to transform
-            var oldHTML = para.innerHTML;
+      // Replace the old HTML with the new HTML
+      para.innerHTML = newHTML
+    })
+  }
+}
 
-            // Wrap the name and timestamp in a span
-            var newHTML = oldHTML.replace(nameAndTimestampRegex, '<span class="name-and-timestamp">$1</span>');
+// Formatting for HTML transcripts from Transistor.
+function formatTranscriptLabels () {
+  'use strict'
 
-            // Wrap the timestamp in its own span
-            var newHTML = newHTML.replace(timestampRegex, '<span class="timestamp">$1</span>');
+  // Kramdown wraps each <cite> in a <p>.
+  // We want to remove that and wrap each pair
+  // of <cite> and <time> in a <div>, so that
+  // we can lay them out neatly.
 
-            // Replace the old HTML with the new HTML
-            para.innerHTML = newHTML;
-        });
-    }
+  if (document.body.getAttribute('data-episode', 'true')) {
+    const citeElements = document.querySelectorAll('cite')
+    citeElements.forEach(function (citeElement) {
+      const citeParagraph = citeElement.closest('p')
+      const citeDiv = document.createElement('div')
+      citeDiv.classList.add('name-and-timestamp')
+      citeDiv.appendChild(citeElement)
+      citeParagraph.insertAdjacentElement('beforebegin', citeDiv)
+      citeParagraph.remove()
 
+      // Move the <time> into the div, too.
+      const timeElement = citeDiv.nextElementSibling
+      if (timeElement.tagName === 'TIME') {
+        citeDiv.appendChild(timeElement)
+      }
+
+      // Let us hide the colon visually for neat design.
+      citeElement.innerHTML = citeElement.innerHTML.replace(':', '<span class="cite-colon">:</span>')
+    })
+  }
 }
 
 // Go
-formatTranscriptHeads();
+formatOldTranscriptHeads()
+formatTranscriptLabels()
